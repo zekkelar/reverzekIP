327c8b542dcc6866c22a5916632f0c10f709031a
Kyurazz <40993331+KyuRazz@users.noreply.github.com>
2020-04-30 04:59:31 +0700

    Add files via upload

---
+++ get_list.py
@@ -0,0 +1,40 @@
+import socket, os, sys
+import requests
+import threading
+from multiprocessing.dummy import Pool
+import os,sys,time
+os.path.exists('Sites')
+import re
+from colorama import Fore								
+from colorama import Style								
+from colorama import init	
+def ip(i):
+	i = i.strip()
+	i = i.replace("http://", "")
+	i = i.replace("https://", "")
+	i = i.replace("/", "")
+	i = i.replace("\n", "")
+	data = socket.gethostbyname(i)
+	ok.append(data)
+def runip():
+	ThreadPool = Pool(500)
+	Threads = ThreadPool.map(ip, ooo)
+
+if __name__ == "__main__":
+	ooo = input('list : ')
+	ooo = open(ooo,'r')
+	kontol = input('save : ')
+	ok = []
+	print('Please Wait...')
+	
+	try:
+		runip()
+	except:
+		pass
+
+	ok = set(ok)
+	ok = list(ok)
+	for hh in ok:
+		print ('[$$$]  '+hh)
+		with open(kontol,'a') as it:
+			it.writelines(hh+'\n')
